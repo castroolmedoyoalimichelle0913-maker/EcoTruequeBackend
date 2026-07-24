a559d0eec18fa720ff82783a9824a60b844bda92
yoa09 <2330537@upt.edu.mx>
2026-07-23 23:54:39 -0600

    Usar variables seguras para MySQL

--- src/main/kotlin/database/DatabaseFactory.kt
+++ src/main/kotlin/database/DatabaseFactory.kt
@@ -7,6 +7,7 @@ import com.example.tables.Usuarios
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.Application
+import io.ktor.server.application.log
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -15,38 +16,55 @@ object DatabaseFactory {
 
     fun init(application: Application) {
 
-        val hikari = HikariConfig().apply {
+        val host = System.getenv("MYSQLHOST")
+            ?: error("Falta la variable MYSQLHOST")
 
+        val port = System.getenv("MYSQLPORT")
+            ?: error("Falta la variable MYSQLPORT")
+
+        val database = System.getenv("MYSQLDATABASE")
+            ?: error("Falta la variable MYSQLDATABASE")
+
+        val user = System.getenv("MYSQLUSER")
+            ?: error("Falta la variable MYSQLUSER")
+
+        val password = System.getenv("MYSQLPASSWORD")
+            ?: error("Falta la variable MYSQLPASSWORD")
+
+        val hikari = HikariConfig().apply {
             driverClassName = "com.mysql.cj.jdbc.Driver"
 
-            jdbcUrl = "jdbc:mysql://thomas.proxy.rlwy.net:50978/railway"
+            jdbcUrl =
+                "jdbc:mysql://$host:$port/$database" +
+                        "?useSSL=false" +
+                        "&allowPublicKeyRetrieval=true" +
+                        "&serverTimezone=UTC"
 
-            username = "root"
-
-            password = "gRMjobFycbqNvwiuJgDcdlmznrpWBVaL"
+            username = user
+            this.password = password
 
             maximumPoolSize = 5
-
+            minimumIdle = 1
+            connectionTimeout = 30_000
             isAutoCommit = false
-
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
 
             validate()
         }
 
-        Database.connect(HikariDataSource(hikari))
+        val dataSource = HikariDataSource(hikari)
+
+        Database.connect(dataSource)
 
         transaction {
-
             SchemaUtils.create(
                 Usuarios,
                 Materiales,
                 Recompensas,
                 Trueques
             )
-
         }
 
-        println("✅ Base de datos conectada.")
+        application.log.info("Base de datos conectada correctamente")
     }
 }
